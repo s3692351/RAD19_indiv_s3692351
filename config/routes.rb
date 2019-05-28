0f2cfde1e2513bcd1fcde3f686574fa7f6dd7331
Sogyal.T.Sherpa <s3692351@student.rmit.edu.au>
2019-05-28 16:18:09 +1000

    Added 404 and Email Config

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   resources :courses do
     resources :likes
   end
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 end
